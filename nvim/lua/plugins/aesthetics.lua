84a3792fc32671332a8fdbfb561edffeb04b6b47
rishavchakra <rishavchakra@gmail.com>
2023-06-09 14:13:21 -0500

    Treesitter context

--- nvim/lua/plugins/aesthetics.lua
+++ nvim/lua/plugins/aesthetics.lua
@@ -33,8 +33,12 @@ return {
     },
     {
         'nvim-treesitter/nvim-treesitter-context',
+        event = 'VeryLazy',
         dependencies = {
             'nvim-treesitter/nvim-treesitter',
-        }
+        },
+        opts = {
+            max_lines = 2,
+        },
     },
 }
