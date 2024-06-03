af4105427dac013a9914daf498e86e855c311a96
rishavchakra <rishavchakra@gmail.com>
2024-06-03 01:04:09 -0500

    Bugfix: fish interactive commands in non-interactive setting

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    starship init fish | source
+
+    zoxide init fish | source
+
+    macchina
 end
 
-alias vim 'nvim'
-alias vi 'nvim'
-alias v 'nvim'
+alias vim nvim
+alias vi nvim
+alias v nvim
 
 alias ls 'exa --icons'
 alias neofetch 'neofetch | lolcat'
@@ -12,12 +17,6 @@ alias neofetch 'neofetch | lolcat'
 alias tmux-go 'tmux attach -t'
 alias tmux-kill 'tmux kill-session'
 
-alias lg 'lazygit'
+alias lg lazygit
 
-set -Ux LIBRARY_PATH '/opt/homebrew/lib'
-
-starship init fish | source
-
-zoxide init fish | source
-
-macchina
+set -Ux LIBRARY_PATH /opt/homebrew/lib
