c60cb9574796c7a4027516e817705f493af43f26
rishavchakra <rishavchakra@gmail.com>
2025-08-13 15:08:36 -0500

    Cursed Linux dotfiles

---
+++ fish/conf.d/bob.fish
@@ -0,0 +1,1 @@
+source "/home/rishavc/.local/share/bob/env/env.fish"
--- nvim/nvim_fix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /opt/homebrew/Cellar/neovim/0.10.0/share/
-mkdir nvim_back
-mv nvim/runtime/ftplugin/zig.vim nvim_back/zig.vim
